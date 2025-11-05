6fc030a4dcf72e4925c13f01b20e91ad186fae36
NEO <sv_Gvozdj@mail.ru>
2025-11-05 11:15:36 +0300

    add @external alias

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,5 +5,6 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@core', dirname(dirname(__DIR__)) . '/core');
+Yii::setAlias('@external', dirname(dirname(__DIR__)) . '/external');
 Yii::setAlias('@upload', dirname(dirname(__DIR__)) . '/frontend/web/upload');
 Yii::setAlias('@root', dirname(dirname(__DIR__)));
